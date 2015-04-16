6de938204b312eb237771234a7db8624c3b5c78e
Harsha Bharadwaj <harsha.bajju@gmail.com>
2015-04-16 17:16:45 +0530

    Storing Batting and Bowling stats in DataFrames

--- WorldCup2015Stats.py
+++ WorldCup2015Stats.py
@@ -1,108 +1,62 @@
-from bs4 import BeautifulSoup
+import bs4
 from pandas import DataFrame
 import requests
 
-def getDOM(url):
+def getDOM(url, parser):
     resp = requests.get(url)
-    return BeautifulSoup(resp.text)
-
-def getDOMParser(url):
-    resp = requests.get(url)
-    return BeautifulSoup(resp.text,"html.parser")
-    
-def getBatsmenInfo(battingTable):
-    batsmenList = []
-    batsmenList.append([batsman.get_text() for batsman in battingTable.select('tr td[class="batsman-name"]')])
-    info = [info.get_text() for info in battingTable.select('tr td + td + td + td')]
-    batsmenList.append(info[0::6][:-2])
-    batsmenList.append(info[2::6][:-1])
-    batsmenList.append(info[3::6][:-1])
-    batsmenList.append(info[4::6][:-1])
-    return batsmenList
-
-def getMatchBattingStats(matchDOM):
-    result = matchDOM.select('.innings-requirement')[0].get_text()
-    date = " ".join(matchDOM.select('.match-information div:nth-of-type(3)')[0].get_text().split()[:3])
-    battingTables = matchDOM.select('table[class~="batting-table"]')
-    team1 = battingTables[0].select('.tr-heading .th-innings-heading')[0].get_text().split('innings')[0]
-    team2 = battingTables[1].select('.tr-heading .th-innings-heading')[0].get_text().split('innings')[0]
-    matchDict = {"team1" : team1, "team2" : team2, "batting1" : getBatsmenInfo(battingTables[0]), "batting2" : getBatsmenInfo(battingTables[1]), 'Result' : result, 'Date' : date}
-    return matchDict
-
-def renderBatting(batting):
-    for i in range(len(batting[0])):
-        print "%s Runs : %s Balls : %s Fours : %s Sixes : %s"%(batting[0][i],batting[1][i],batting[2][i],batting[3][i],batting[4][i])
-def getLine():
-    return "-----------------------------------"    
-        
-def getBowlerInfo(bowlingTable):
-    bowlerList = []
-    bowlerList.append([bowler.get_text() for bowler in bowlingTable.select('tr td[class="bowler-name"]')])
-    bowlerList.append([info.get_text() for info in bowlingTable.select('tr td[class="bowler-name"] + td')])
-    bowlerList.append([info.get_text() for info in bowlingTable.select('tr td[class="bowler-name"] + td + td + td')])
-    bowlerList.append([info.get_text() for info in bowlingTable.select('tr td[class="bowler-name"] + td + td + td + td')])
-    return bowlerList
-
-def renderBowling(bowling):
-    for i in range(len(bowling[0])):
-        print "%s Overs : %s Runs Given : %s Wickets Taken : %s"%(bowling[0][i],bowling[1][i],bowling[2][i],bowling[3][i])
-
-def getMatchBowlingStats(matchDOM, matchDict):
-    bowlingTables = matchDOM.select("table.bowling-table")  
-    matchDict["bowling1"] = (getBowlerInfo(bowlingTables[0]))
-    matchDict["bowling2"] = (getBowlerInfo(bowlingTables[1]))
-    return matchDict
-
-def render(matchInfo):
-    print getLine()
-    print "Date : %s" %matchInfo["Date"]
-    print getLine()
-    print "Result : %s" %matchInfo["Result"]
-    print matchInfo["team1"]
-    print "Batting STATS"
-    print getLine()
-    renderBatting(matchInfo["batting1"])
-    print getLine()
-    print "Bowling STATS"
-    renderBowling(matchInfo["bowling2"])
-    print getLine()
-    print matchInfo["team2"] 
-    print getLine() 
-    print "Batting Stats"
-    renderBatting(matchInfo["batting2"])
-    print getLine()
-    print "Bowling STATS"
-    renderBowling(matchInfo["bowling1"])
-    
-      
+    return bs4.BeautifulSoup(resp.text, parser)
+def getInningsSummary():
+    pass
+def getBattingScore(matchDOM, innings, matchNum):
+    battingTables = matchDOM.select('table.batting-table')
+    battingLine = []
+    country = battingTables[innings - 1].select('th.th-innings-heading')[0].text.split('innings')[0].strip()
+    for element in battingTables[innings - 1].select('tr td.batsman-name'):
+        row = [matchNum, country, element.get_text().strip()]
+        for i in range(7):
+            element = element.next_sibling.next_sibling
+            if element == None:
+                row += [element]
+            else:
+                row += [element.get_text().strip()]
+        battingLine.append(row)
+    return battingLine
+def getBowlingScore(matchDOM, innings, country, matchNum):
+    bowlingTables = matchDOM.select('table.bowling-table')
+    bowlingLine = []
+    for element in bowlingTables[innings - 1].select('tr td.bowler-name'):
+        row = [matchNum, country, element.get_text().strip()]
+        for i in range(9):
+            element = element.next_sibling.next_sibling
+            if element == None:
+                row += [element]
+            else:
+                row += [element.get_text()]
+        bowlingLine.append(row)        
+    return bowlingLine
 
 espnHomeURL = 'http://www.espncricinfo.com'
 wcResultsURL = '/icc-cricket-world-cup-2015/engine/series/509587.html'
-wcResultsDOM = getDOMParser(espnHomeURL + wcResultsURL)
-
+wcResultsDOM = getDOM(espnHomeURL + wcResultsURL, "html.parser")
+#inningsSummary = matchResultsDOM.select('div.brief-summary div')
+#matchResult = inningsSummary[3].get_text()
+#matchDate = inningsSummary[7].get_text().strip().split('(')[0]
 matchLinks = [link['href'] for link in wcResultsDOM.select('div[class~="news-list"] div[class~="content_data"] a[class~="potMatchLink"]')]
-
-matchesInfo = {}
-
+batting = []
+bowling = []
 for index, matchLink in enumerate(matchLinks):
     if index != 10:
-        matchURL = espnHomeURL + matchLink
-        print "loading match %d ........."%(index + 1)
-        matchDOM= getDOM(matchURL)
-        matchesInfo[index + 1] = getMatchBowlingStats(matchDOM,getMatchBattingStats(matchDOM))
-    else:
-        matchesInfo[index + 1] = "Australia V Bangladesh Match Cancelled Due to Rain!!!"
+        print index
+        matchResultsDOM = getDOM(espnHomeURL + matchLink, "lxml")
+        battingScore1 = getBattingScore(matchResultsDOM, 1, index + 1)
+        battingScore2 = getBattingScore(matchResultsDOM, 2, index + 1)
+        bowlingScore1 = getBowlingScore(matchResultsDOM, 1, battingScore2[0][1], index + 1)
+        bowlingScore2 = getBowlingScore(matchResultsDOM, 2, battingScore1[0][1], index + 1)
+        batting += battingScore1 + battingScore2
+        bowling += bowlingScore1 + bowlingScore2
 
-df = DataFrame(matchesInfo)
-print df    
-matchNum = int(raw_input("Enter Match Number to see the stats : "))
-
-while matchNum != 0:
-    if matchNum != 11:
-        render(matchesInfo[matchNum])
-    else:
-        print matchesInfo[11]
-    matchNum = int(raw_input("Enter Match Number to see the stats press 0 to exit : "))
-
-
-
+battingDF = DataFrame(batting, columns = ['MatchNum', 'Country', 'Batsman','Dismissal-Info','R','M','B','4s','6s', 'SR'])
+bowlingDF = DataFrame(bowling, columns = ['MatchNum', 'Country','Bowler','O','M','R','W','Econ','0s','4s','6s','Extra'])
+print battingDF
+print bowlingDF
+    
